9157f0490a8266d79ef97b4eb1e3c55f28b7bc7f
lorddc1989 <68823409+lorddc1989@users.noreply.github.com>
2023-03-28 12:26:03 +0100

    Added Get-CWMInvoices function

---
+++ ConnectWiseManageAPI/Public/Finance/Invoices/Get-CWMInvoices.ps1
@@ -0,0 +1,18 @@
+function Get-CWMInvoices {
+    [CmdletBinding()]
+    param(
+        [int]$id,
+        [switch]$count,
+        [string]$condition,
+        [ValidatePattern('\S* (desc|asc)')][string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all
+    )
+
+    $Endpoint = '/finance/invoices'
+    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
